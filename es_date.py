aa37daf080bf7a1117e66e41b52996fbba0072ee
Matteo <matteo.pelusi@studenti.isissgobetti.it>
2023-03-23 13:50:50 +0100

    esercizi date

---
+++ es_date.py
@@ -0,0 +1,13 @@
+#es Date
+#Pelusi Matteo 3M
+
+#1 stampare la dara corrente
+import datetime
+date = datetime.datetime.now()
+print("Data e ora :", date)
+
+#2 creare data conpleanno
+compleanno = datetime.datetime(2006,11,6)
+print("Compleanno:",compleanno)
+
+#3 stampare data
